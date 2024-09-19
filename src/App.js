f0a48029f37397ed661847c88f1f6a7eb25f6114
Kulam9 <kulam.521587589@gmail.com>
2024-09-19 09:17:23 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './app.module.css';
+import data from './data.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+	const [steps, setStaps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-export default App;
+	const handleNext = () => {
+		setActiveIndex((prevState) => {
+			return prevState + 1;
+		});
+	};
+
+	const handleBack = () => {
+		if (activeIndex > 0) {
+			setActiveIndex((prevState) => {
+				return prevState - 1;
+			});
+		}
+	};
+
+	const handleReset = () => {
+		setActiveIndex(0);
+	};
+
+	const isFirstStep = activeIndex === 0;
+	const isLastStep = activeIndex === steps.length - 1;
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{/* Для получения активного контента использйте steps и activeIndex */}
+						{steps[activeIndex].content};
+						{/* Контент соответственный шагу. Сейчас активен шаг 3 */}
+					</div>
+					<ul className={styles['steps-list']}>
+						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
+						{steps.map((step, index) => {
+							return (
+								<li
+									className={
+										styles['steps-item'] +
+										(activeIndex === index
+											? ` ${styles.active}`
+											: '') +
+										(index < activeIndex ? ` ${styles.done}` : '')
+									}
+								>
+									{/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
+
+									<button className={styles['steps-item-button']}>
+										{index + 1}
+									</button>
+									{/* При клике на кнопку установка выбранного шага в качестве активного */}
+									{step.title}
+								</li>
+							);
+						})}
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button className={styles.button} onClick={handleBack}>
+							Назад
+						</button>
+						<button
+							className={styles.button}
+							onClick={() => (isLastStep ? handleReset() : handleNext())}
+						>
+							{isLastStep ? 'Начать с начала' : 'Далее'}
+							{/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
+							{/* Или заменять всю кнопку в зависимости от условия */}
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
